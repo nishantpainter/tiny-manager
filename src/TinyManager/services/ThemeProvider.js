b5ad4fe4764766b3a1aae928845bf55785b77833
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-08-20 20:55:51 +0530

    Add theme provider

---
+++ src/TinyManager/services/ThemeProvider.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import indigo from "@material-ui/core/colors/indigo";
+import orange from "@material-ui/core/colors/orange";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: indigo,
+    secondary: orange,
+  },
+});
+
+function ThemeProvider(props) {
+  const { children } = props;
+  return (
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </MuiThemeProvider>
+  );
+}
+
+export default ThemeProvider;
