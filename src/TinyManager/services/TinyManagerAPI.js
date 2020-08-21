e9178818e37237696b549fc33641c76a72f15f70
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-08-22 00:09:20 +0530

    Add tiny manager api stub

---
+++ src/TinyManager/services/TinyManagerAPI.js
@@ -0,0 +1,32 @@
+function fetchProjects() {}
+
+function fetchProject(projectId) {}
+
+function fetchTasks(projectId) {}
+
+function fetchTask(taskId) {}
+
+function addProject(project) {}
+
+function addTask(task) {}
+
+function updateProject(projectId, project) {}
+
+function updateTask(taskId, task) {}
+
+function removeProject(projectId) {}
+
+function removeTask(taskId) {}
+
+export default {
+  fetchProjects,
+  fetchProject,
+  fetchTasks,
+  fetchTask,
+  addProject,
+  addTask,
+  updateProject,
+  updateTask,
+  removeProject,
+  removeTask,
+};
