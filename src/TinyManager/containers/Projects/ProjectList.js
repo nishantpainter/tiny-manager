55950fd8704cdfcc48cd08432d404e541fbb4d7f
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-08-22 00:49:56 +0530

    Add project list

--- src/TinyManager/containers/Projects/ProjectList.js
+++ src/TinyManager/containers/Projects/ProjectList.js
@@ -1,8 +1,35 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
+import ProjectCard from "TinyManager/components/ProjectCard";
+import TinyManagerAPI from "TinyManager/services/TinyManagerAPI";
+import Grid from "@material-ui/core/Grid";
 
 function ProjectList() {
-  return <Typography variant="h1">Project List</Typography>;
+  const [projects, setProjects] = React.useState([]);
+
+  React.useEffect(() => {
+    TinyManagerAPI.fetchProjects().then(setProjects);
+  }, []);
+
+  return (
+    <>
+      <Typography variant="h1" gutterBottom>
+        Project List
+      </Typography>
+      <br />
+      {projects && projects.length ? (
+        <Grid container spacing={3}>
+          {projects.map((project) => (
+            <Grid key={project.id} item xs={12} sm={6}>
+              <ProjectCard project={project} />
+            </Grid>
+          ))}
+        </Grid>
+      ) : (
+        <Typography variant="body1">No available projects.</Typography>
+      )}
+    </>
+  );
 }
 
 export default ProjectList;
