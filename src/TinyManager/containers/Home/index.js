615f7b46d0656bf71e5254662315ec85be8aa028
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-08-25 18:59:41 +0530

    Implement quote feature

--- src/TinyManager/containers/Home/index.js
+++ src/TinyManager/containers/Home/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
 
+import QuoteService from "TinyManager/services/QuoteService";
+
 function Home() {
+  const quote = QuoteService.getQuote();
+
   return (
-    <Typography variant="h5">
-      Home
-    </Typography>
+    <>
+      <Typography variant="h5" gutterBottom>
+        Home
+      </Typography>
+      <Typography variant="body1" color="textSecondary">
+        {quote}
+      </Typography>
+    </>
   );
 }
 
