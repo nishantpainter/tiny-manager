8e498250f94bd537ddef9a635382a12b7b247df0
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-08-29 12:05:31 +0530

    Update structure

--- src/TinyManager/components/Paper/Paper.stories.js
+++ src/TinyManager/components/Paper/Paper.stories.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Paper from "TinyManager/components/Paper";
+import Paper from "./Paper";
 import ThemeProvider from "TinyManager/services/ThemeProvider";
 
 export default {
--- src/TinyManager/components/Paper/index.js
+++ src/TinyManager/components/Paper/index.js
@@ -1,24 +1,3 @@
-import PropTypes from "prop-types";
-import MuiPaper from "@material-ui/core/Paper";
-import { withStyles } from "@material-ui/core/styles";
-
-const Paper = withStyles((theme) => ({
-  root: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    cursor: "pointer",
-    boxShadow: theme.custom.shadow[0],
-    borderRadius: theme.spacing(0.5),
-    padding: theme.spacing(),
-    height: theme.spacing(15),
-  },
-}))(MuiPaper);
-
-Paper.displayName = "Paper";
-
-Paper.propTypes = {
-  className: PropTypes.string,
-};
+import Paper from './Paper';
 
 export default Paper;
