7a1cad3cd933a8f4adc61a65b7de2752f2d071b7
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-09-01 19:12:32 +0530

    Add new task button

--- src/TinyManager/containers/Projects/ProjectView.js
+++ src/TinyManager/containers/Projects/ProjectView.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Typography, Grid, Fade } from "@material-ui/core";
+import { Typography, Grid, Button, Fade } from "@material-ui/core";
 
 import ProjectCard from "TinyManager/components/ProjectCard";
 import TinyManagerAPI from "TinyManager/services/TinyManagerAPI";
@@ -28,6 +28,8 @@ function ProjectView(props) {
     }
   }, [projectId, redirectToProjectList]);
 
+  const handleNewTask = React.useCallback(() => {}, []);
+
   if (loading) {
     return <Loader />;
   }
@@ -42,6 +44,11 @@ function ProjectView(props) {
         <Grid container spacing={2}>
           <Grid item xs={12}>
             <ProjectCard project={project} />
+          </Grid>
+          <Grid item xs={12}>
+            <Button color="primary" variant="outlined" onClick={handleNewTask}>
+              Add New Task
+            </Button>
           </Grid>
         </Grid>
       </div>
