8389151cdfe09c6fb8bb2cd19b54fa32d03bfce5
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-09-03 19:06:55 +0530

    Add todo list

---
+++ src/TinyManager/components/TodoList/TodoList.js
@@ -0,0 +1,37 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { List, Typography } from "@material-ui/core";
+
+import Todo from "../Todo";
+
+function TodoList(props) {
+  const { todos, onTodoClick } = props;
+
+  if (todos.length === 0) {
+    return <Typography>Nothing to be completed. Enjoy your day.</Typography>;
+  }
+
+  return (
+    <List>
+      {todos.map((todo) => (
+        <Todo
+          key={todo.id}
+          title={todo.title}
+          isCompleted={todo.isCompleted}
+          onClick={onTodoClick}
+        />
+      ))}
+    </List>
+  );
+}
+
+TodoList.prototype = {
+  onTodoClick: PropTypes.func,
+};
+
+TodoList.defaultProps = {
+  todos: [],
+  onTodoClick: () => {},
+};
+
+export default TodoList;
