538c32bb73ce66ec4b163b16dbfe0b5446a101fa
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-09-03 19:24:23 +0530

    Add dark mode stories

--- src/TinyManager/components/TodoList/TodoList.stories.js
+++ src/TinyManager/components/TodoList/TodoList.stories.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import TodoList from "./TodoList";
+import ThemeProvider from "TinyManager/services/ThemeProvider";
 
 const argTypes = {
   todos: {
@@ -45,4 +46,22 @@ Default.args = {
 export const Empty = Template.bind({});
 Empty.args = {
   todos: [],
+};
+
+const DarkTemplate = (args) => {
+  return (
+    <ThemeProvider darkMode={true}>
+      <TodoList {...args} />
+    </ThemeProvider>
+  );
+};
+
+export const DarkModeDefault = DarkTemplate.bind({});
+DarkModeDefault.args = {
+  ...Default.args,
+};
+
+export const DarkModeEmpty = DarkTemplate.bind({});
+DarkModeEmpty.args = {
+  ...Empty.args,
 };
