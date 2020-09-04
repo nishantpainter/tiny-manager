68a4472ddf2e5fbc3ba6b6b9ed43a442368f6185
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-09-04 20:08:03 +0530

    Initialized database

--- src/TinyManager/services/DbService.js
+++ src/TinyManager/services/DbService.js
@@ -1,3 +1,22 @@
+import Dexie from "dexie";
+
+const db = new Dexie("TinyManager");
+
+function initateDB() {
+  db.version(1).stores({
+    projects: "++id,name",
+  });
+
+  db.projects.defineClass({ name: String, description: String });
+}
+
+function getTable(tableName) {
+  const table = db.table(tableName);
+  return {
+    name: table.name,
+  };
+}
+
 function find() {}
 
 function findOne() {}
@@ -8,10 +27,13 @@ function update() {}
 
 function remove() {}
 
+initateDB();
+
 export default {
   find,
   findOne,
   insert,
   update,
   remove,
+  getTable,
 };
