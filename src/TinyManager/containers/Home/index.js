42e7c763cf5b32bee739f90a45e416e0e005de38
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-09-04 20:55:37 +0530

    Configure todos

--- src/TinyManager/containers/Home/index.js
+++ src/TinyManager/containers/Home/index.js
@@ -2,14 +2,18 @@ import React from "react";
 import { Fade, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
+import Todos from "TinyManager/containers/Todos";
 import QuoteService from "TinyManager/services/QuoteService";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   container: {
     width: "inherit",
     height: "inherit",
   },
-});
+  todos: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
 function Home() {
   const quote = QuoteService.getQuote();
@@ -17,12 +21,10 @@ function Home() {
   return (
     <Fade in={true}>
       <div className={classes.container}>
-        <Typography variant="h5" gutterBottom>
-          Home
-        </Typography>
-        <Typography variant="body1" color="textSecondary">
+        <Typography variant="body1" color="textSecondary" gutterBottom>
           {quote}
         </Typography>
+        <Todos className={classes.todos} />
       </div>
     </Fade>
   );
