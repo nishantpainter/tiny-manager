4c7550e1dbc6e3069b407b5cbdedd51750e94479
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-09-04 21:25:57 +0530

    Add types

---
+++ src/TinyManager/types/index.js
@@ -0,0 +1,11 @@
+import PropTypes from "prop-types";
+
+const project = {
+  id: PropTypes.string,
+  name: PropTypes.string,
+  description: PropTypes.string,
+};
+
+export default {
+  project,
+};
