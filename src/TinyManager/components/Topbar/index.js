4e1ca6c1b4031fb3fb0e82f14c41d9eaf37d3253
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-09-05 12:48:21 +0530

    Organize imports

--- src/TinyManager/components/Topbar/index.js
+++ src/TinyManager/components/Topbar/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
-import MuiAppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
+import {
+  AppBar as MuiAppBar,
+  Toolbar,
+  Button,
+  Typography,
+  IconButton,
+} from "@material-ui/core";
 import { makeStyles, withStyles } from "@material-ui/core/styles";
-import ToggleDarkThemeIcon from "@material-ui/icons/InvertColors";
+import { InvertColors as ToggleDarkThemeIcon } from "@material-ui/icons";
 
 const AppBar = withStyles((theme) => ({
   root: {
