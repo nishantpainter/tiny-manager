e1c6d64421d4ad21d718689db654f4469e54c3d6
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-09-05 13:12:25 +0530

    Update arg type

--- src/TinyManager/components/TodoForm/TodoForm.stories.js
+++ src/TinyManager/components/TodoForm/TodoForm.stories.js
@@ -1,10 +1,73 @@
 import React from "react";
 
 import TodoForm from "./TodoForm";
+import { TodoType } from "TinyManager/types";
+
+const argTypes = {
+  title: {
+    label: "title",
+    description: "Title of form",
+    type: { name: "string" },
+    control: {
+      type: "string",
+    },
+  },
+  disabled: {
+    label: "disabled",
+    description: "Disable state of form",
+    type: {
+      name: "boolean",
+    },
+    control: {
+      type: "boolean",
+    },
+  },
+  values: {
+    label: "values",
+    description: "Values of the form",
+    type: TodoType,
+    control: {
+      type: null,
+    },
+  },
+  errors: {
+    label: "errors",
+    description: "Errors of the form",
+    type: TodoType,
+    control: {
+      type: null,
+    },
+  },
+  onChange: {
+    label: "onChange",
+    description: "Form change handler",
+    type: "func",
+    control: {
+      type: null,
+    },
+  },
+  onCancel: {
+    label: "onCancel",
+    description: "Form cancel handler",
+    type: "func",
+    control: {
+      type: null,
+    },
+  },
+  onSubmit: {
+    label: "onSubmit",
+    description: "Form submit handler",
+    type: "func",
+    control: {
+      type: null,
+    },
+  },
+};
 
 export default {
   title: "Example/TodoForm",
   component: TodoForm,
+  argTypes,
 };
 
 const Template = (args) => <TodoForm {...args} />;
