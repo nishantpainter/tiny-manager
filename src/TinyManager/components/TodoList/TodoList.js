b69242cfa4ecb7c5b640a285181a731212232ffe
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-09-05 12:33:48 +0530

    Update todo list style

--- src/TinyManager/components/TodoList/TodoList.js
+++ src/TinyManager/components/TodoList/TodoList.js
@@ -1,19 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
 import { List, Typography } from "@material-ui/core";
 
 import Todo from "../Todo";
 import { TodoType } from "TinyManager/types";
 
+const useStyles = makeStyles({
+  list: {
+    overflow: "auto",
+    height: "95%",
+  },
+});
+
 function TodoList(props) {
   const { todos, onTodoClick } = props;
+
+  const classes = useStyles();
 
   if (todos.length === 0) {
     return <Typography>Nothing to be completed. Enjoy your day.</Typography>;
   }
 
   return (
-    <List>
+    <List className={classes.list}>
       {todos.map((todo) => (
         <Todo key={todo.id} todo={todo} onClick={onTodoClick} />
       ))}
