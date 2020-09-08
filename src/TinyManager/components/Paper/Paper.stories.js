40bab7a86cad6973657ac91be9798f09f5d85112
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-09-08 19:56:09 +0530

    Remove theme provider

--- src/TinyManager/components/Paper/Paper.stories.js
+++ src/TinyManager/components/Paper/Paper.stories.js
@@ -1,18 +1,13 @@
 import React from "react";
 
 import Paper from "./Paper";
-import ThemeProvider from "TinyManager/services/ThemeProvider";
 
 export default {
   title: "Example/Paper",
   component: Paper,
 };
 
-const Template = (args) => (
-  <ThemeProvider>
-    <Paper {...args} />
-  </ThemeProvider>
-);
+const Template = (args) => <Paper {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {};
