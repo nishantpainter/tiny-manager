4e9d35c9dfb81bc4a8182160cf25f13c0b12b51b
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-09-08 19:56:31 +0530

    Add story

---
+++ src/TinyManager/components/ProjectCard/ProjectCard.stories.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import ProjectCard from "./ProjectCard";
+
+export default {
+  title: "Example/ProjectCard",
+  component: ProjectCard,
+};
+
+const Template = (args) => <ProjectCard {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  project: {
+    name: "Tiny Manager",
+    description: "Tiny manager for your self project",
+  },
+};
+
+export const Progress = Template.bind({});
+Progress.args = {
+  ...Default.args,
+  progress: 100,
+};
