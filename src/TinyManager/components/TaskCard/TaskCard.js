864ed8b1cca5ab5138fe92aa7169eff21977c2f9
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-09-12 14:23:21 +0530

    Add task progress

--- src/TinyManager/components/TaskCard/TaskCard.js
+++ src/TinyManager/components/TaskCard/TaskCard.js
@@ -1,18 +1,18 @@
 import React from "react";
 import clsx from "clsx";
 import PropTypes from "prop-types";
-import { Typography, IconButton } from "@material-ui/core";
+import { Typography, IconButton, Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 import DeleteIcon from "@material-ui/icons/Delete";
 
 import Paper from "../Paper";
 import { TaskType } from "TinyManager/types/index";
+import CircularProgressWithLabel from "../CircularProgressWithLabel";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
     height: theme.spacing(7),
-    justifyContent: "space-between",
     alignItems: "center",
   },
   mediumPriority: {
@@ -21,6 +21,11 @@ const useStyles = makeStyles((theme) => ({
   highPriority: {
     backgroundColor: theme.palette.error.light,
   },
+  progress: {
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
 }));
 
 function TaskCard(props) {
@@ -28,6 +33,7 @@ function TaskCard(props) {
   const { priority } = task;
 
   const classes = useStyles();
+
   return (
     <Paper
       classes={{
@@ -39,10 +45,22 @@ function TaskCard(props) {
       }}
       onClick={onClick}
     >
-      <Typography>{task.title}</Typography>
-      <IconButton size="small" onDelete={onDelete}>
-        <DeleteIcon />
-      </IconButton>
+      <Grid container alignItems="center">
+        <Grid item xs={9}>
+          <Typography>{task.title}</Typography>
+        </Grid>
+        <Grid item xs={2} align="right" className={classes.progress}>
+          <CircularProgressWithLabel
+            color={priority === 0 ? "primary" : "secondary"}
+            value={task.progress}
+          />
+        </Grid>
+        <Grid item xs={1} align="right">
+          <IconButton onDelete={onDelete}>
+            <DeleteIcon />
+          </IconButton>
+        </Grid>
+      </Grid>
     </Paper>
   );
 }
