cd98adc734f9b0ca32d6b9800d1d65b5040cc20d
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-09-12 13:38:35 +0530

    Add task card story

---
+++ src/TinyManager/components/TaskCard/TaskCard.stories.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import TaskCard from "./TaskCard";
+
+export default {
+  title: "TinyManager/TaskCard",
+  component: TaskCard,
+  decorators: [
+    (Story) => (
+      <div style={{ width: 500 }}>
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+const Template = (args) => <TaskCard {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  task: {
+    title: "Initate production",
+  },
+};
