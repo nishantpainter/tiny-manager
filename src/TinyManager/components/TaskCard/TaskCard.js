fe41db78be98e914518f881ada5f84fec7af3ece
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-09-12 13:47:21 +0530

    Update background based on priority

--- src/TinyManager/components/TaskCard/TaskCard.js
+++ src/TinyManager/components/TaskCard/TaskCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import clsx from "clsx";
 import PropTypes from "prop-types";
 import { Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
@@ -11,14 +12,30 @@ const useStyles = makeStyles((theme) => ({
     height: theme.spacing(7),
     justifyContent: "flex-start",
   },
+  mediumPriority: {
+    backgroundColor: theme.palette.warning.light,
+  },
+  highPriority: {
+    backgroundColor: theme.palette.error.light,
+  },
 }));
 
 function TaskCard(props) {
   const { task, onClick } = props;
+  const { priority } = task;
 
   const classes = useStyles();
   return (
-    <Paper className={classes.paper} onClick={onClick}>
+    <Paper
+      classes={{
+        root: clsx(
+          classes.paper,
+          priority === 1 && classes.mediumPriority,
+          priority === 2 && classes.highPriority
+        ),
+      }}
+      onClick={onClick}
+    >
       <Typography>{task.title}</Typography>
     </Paper>
   );
