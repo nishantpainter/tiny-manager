1fd31095c59b93d3fd1d8a5ce3c9a60c5e054ed1
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-09-27 10:34:18 +0530

    Update types

--- src/TinyManager/types/index.js
+++ src/TinyManager/types/index.js
@@ -1,21 +1,21 @@
 import PropTypes from "prop-types";
 
 export const ProjectType = PropTypes.shape({
-  id: PropTypes.string,
+  id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   name: PropTypes.string,
   description: PropTypes.string,
 });
 
 export const TaskType = PropTypes.shape({
-  id: PropTypes.string,
+  id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   title: PropTypes.string,
   note: PropTypes.string,
   priority: PropTypes.number,
-  progress:PropTypes.number
+  progress: PropTypes.number,
 });
 
 export const TodoType = PropTypes.shape({
-  id: PropTypes.string,
+  id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   title: PropTypes.string,
   completed: PropTypes.bool,
 });
