92cbacaacb0f0f163947f03fa25d6609f2d54066
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-10-09 19:11:52 +0530

    Add icon button

---
+++ src/TinyManager/components/IconButton/IconButton.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+import MuiIconButton from "@material-ui/core/IconButton";
+
+import FilterListIcon from "@material-ui/icons/FilterList";
+
+const icons = {
+  filter: FilterListIcon,
+};
+
+function IconButton(props) {
+  const { icon, ...rest } = props;
+  const Icon = icons[icon];
+  return (
+    <MuiIconButton size="small" {...rest}>
+      <Icon />
+    </MuiIconButton>
+  );
+}
+
+IconButton.propTypes = {
+  /** Icon to be displayed */
+  icon: PropTypes.string,
+};
+
+export default IconButton;
