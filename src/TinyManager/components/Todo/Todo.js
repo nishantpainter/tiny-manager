c5a7f802993175152a3671775828d45c060dddd9
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-10-25 08:24:19 +0530

    Fix imports

--- src/TinyManager/components/Todo/Todo.js
+++ src/TinyManager/components/Todo/Todo.js
@@ -1,12 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  ListItemSecondaryAction,
-  Checkbox,
-} from "@material-ui/core";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import Checkbox from "@material-ui/core/Checkbox";
 import { makeStyles } from "@material-ui/core/styles";
 
 import IconButton from "TinyManager/components/IconButton";
