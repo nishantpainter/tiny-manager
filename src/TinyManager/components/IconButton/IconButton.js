5ccc2a24fbb5eebe6cdb7fc528bf095ba6e55cf7
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-10-25 08:12:41 +0530

    Add attachment icon

--- src/TinyManager/components/IconButton/IconButton.js
+++ src/TinyManager/components/IconButton/IconButton.js
@@ -4,10 +4,12 @@ import MuiIconButton from "@material-ui/core/IconButton";
 
 import FilterListIcon from "@material-ui/icons/FilterList";
 import EditIcon from "@material-ui/icons/Edit";
+import AttachFile from "@material-ui/icons/AttachFile";
 
 const icons = {
   filter: FilterListIcon,
   edit: EditIcon,
+  attachment: AttachFile,
 };
 
 function IconButton(props) {
