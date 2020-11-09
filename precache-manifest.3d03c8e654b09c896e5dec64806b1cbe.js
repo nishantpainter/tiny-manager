2201f017225e75ae8d03cacc4519106d2f459289
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-11-09 16:10:58 +0530

    Updates

---
+++ precache-manifest.3d03c8e654b09c896e5dec64806b1cbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84863f147d143adb24b3c66266b71d1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1049c9859aac868153d6",
+    "url": "/static/css/main.8ec763f5.chunk.css"
+  },
+  {
+    "revision": "9edb5e1d640256c31f9a",
+    "url": "/static/js/2.1042724c.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/static/js/2.1042724c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1049c9859aac868153d6",
+    "url": "/static/js/main.077dedbb.chunk.js"
+  },
+  {
+    "revision": "4cc19e00ace887a25424",
+    "url": "/static/js/runtime-main.0a50465f.js"
+  }
+]);
