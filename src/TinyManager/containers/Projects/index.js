c55fb8931a35459221b220c3dde99040bc8fd365
Nishant Painter <virtual.nishantpainter@gmail.com>
2021-03-20 17:46:12 +0530

    Update project style

--- src/TinyManager/containers/Projects/index.js
+++ src/TinyManager/containers/Projects/index.js
@@ -1,15 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Route, Switch } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
 
 import TinyManagerAPI from "TinyManager/services/TinyManagerAPI";
 import ProjectView from "TinyManager/containers/Projects/ProjectView";
 import ProjectList from "TinyManager/containers/Projects/ProjectList";
 import ProjectForm from "TinyManager/containers/Projects/ProjectForm";
 
+const useStyles = makeStyles(() => ({
+  container: {
+    display: "flex",
+    flex: 1,
+    justifyContent: "center",
+  },
+  content: {
+    width: 500,
+  },
+}));
+
 function Projects(props) {
   const { match, history } = props;
   const { path, url } = match;
+
+  const classes = useStyles();
 
   const redirectToNewProject = () => {
     history.push(url + `/new`);
@@ -31,39 +45,41 @@ function Projects(props) {
   );
 
   return (
-    <>
-      <Switch>
-        <Route
-          path={path + "/new"}
-          render={(props) => (
-            <ProjectForm
-              {...props}
-              onCancel={redirectToProjectList}
-              onSubmit={handleAddNewProject}
-            />
-          )}
-        />
-        <Route
-          path={path + "/:projectId"}
-          render={(props) => (
-            <ProjectView
-              {...props}
-              redirectToProjectList={redirectToProjectList}
-            />
-          )}
-        />
-        <Route
-          path={path}
-          render={(props) => (
-            <ProjectList
-              {...props}
-              onProjectClick={redirectToProjectView}
-              onNewProject={redirectToNewProject}
-            />
-          )}
-        />
-      </Switch>
-    </>
+    <div className={classes.container}>
+      <div className={classes.content}>
+        <Switch>
+          <Route
+            path={path + "/new"}
+            render={(props) => (
+              <ProjectForm
+                {...props}
+                onCancel={redirectToProjectList}
+                onSubmit={handleAddNewProject}
+              />
+            )}
+          />
+          <Route
+            path={path + "/:projectId"}
+            render={(props) => (
+              <ProjectView
+                {...props}
+                redirectToProjectList={redirectToProjectList}
+              />
+            )}
+          />
+          <Route
+            path={path}
+            render={(props) => (
+              <ProjectList
+                {...props}
+                onProjectClick={redirectToProjectView}
+                onNewProject={redirectToNewProject}
+              />
+            )}
+          />
+        </Switch>
+      </div>
+    </div>
   );
 }
 
