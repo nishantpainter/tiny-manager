55dc7cb3c6fc266cb1545d8306388db288832c24
Nishant Painter <virtual.nishantpainter@gmail.com>
2021-04-28 19:04:17 +0530

    Configure translation provider

--- src/TinyManager/index.js
+++ src/TinyManager/index.js
@@ -3,12 +3,15 @@ import { HashRouter } from "react-router-dom";
 
 import Content from "TinyManager/containers/Content";
 import ThemeProvider from "TinyManager/services/ThemeProvider";
+import TranslationProvider from "TinyManager/services/TranslationProvider";
 
 function TinyManager() {
   return (
     <ThemeProvider>
       <HashRouter>
-        <Content />
+        <TranslationProvider>
+          <Content />
+        </TranslationProvider>
       </HashRouter>
     </ThemeProvider>
   );
