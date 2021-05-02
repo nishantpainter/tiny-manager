b209fb3cd63ff8b6135322fa3a30c5cabae506d2
Nishant Painter <virtual.nishantpainter@gmail.com>
2021-05-02 11:45:06 +0530

    Centered project form

--- src/TinyManager/containers/Projects/ProjectForm.js
+++ src/TinyManager/containers/Projects/ProjectForm.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { useFormik } from "formik";
 import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
 
 import ProjectForm from "TinyManager/components/ProjectForm";
 import { ProjectType } from "TinyManager/types/index";
 
+const useStyles = makeStyles(() => ({
+  container: {
+    maxWidth: 500,
+    marginRight: "auto",
+    marginLeft: "auto",
+  },
+}));
+
 function ProjectFormContainer(props) {
   const { onCancel, onSubmit, initialValues } = props;
+  const classes = useStyles();
 
   const handleCancel = React.useCallback(() => {
     onCancel();
@@ -41,14 +51,16 @@ function ProjectFormContainer(props) {
   });
 
   return (
-    <ProjectForm
-      values={formik.values}
-      errors={formik.errors}
-      onCancel={handleCancel}
-      onChange={formik.handleChange}
-      onSubmit={formik.handleSubmit}
-      disabled={formik.isSubmitting}
-    />
+    <div className={classes.container}>
+      <ProjectForm
+        values={formik.values}
+        errors={formik.errors}
+        onCancel={handleCancel}
+        onChange={formik.handleChange}
+        onSubmit={formik.handleSubmit}
+        disabled={formik.isSubmitting}
+      />
+    </div>
   );
 }
 
