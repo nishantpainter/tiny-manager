dd4220e0e4bcf735d90071418189d173d4cbb223
Nishant Painter <virtual.nishantpainter@gmail.com>
2021-05-02 11:57:01 +0530

    Add withProps utility

--- src/TinyManager/services/Utils.js
+++ src/TinyManager/services/Utils.js
@@ -1,5 +1,15 @@
+import React from "react";
+
 function join(...strings) {
   return strings.join("");
 }
 
-export { join };
+// eslint-disable-next-line no-unused-vars
+function withProps(Component, props) {
+  // eslint-disable-next-line react/display-name
+  return function ($props) {
+    return <Component {...$props} {...props} />;
+  };
+}
+
+export { join, withProps };
