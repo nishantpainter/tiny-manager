ef3b90e350faee9d9761f8cb9bbb23e1b4fcf2b3
Nishant Painter <virtual.nishantpainter@gmail.com>
2021-05-03 09:21:54 +0530

    Configure topbar translate in container

--- src/TinyManager/containers/Topbar/index.js
+++ src/TinyManager/containers/Topbar/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Topbar from "TinyManager/components/Topbar";
 import { useTheme } from "TinyManager/providers/ThemeProvider";
+import { useTranslation } from "TinyManager/providers/TranslationProvider";
 
 function TopbarContainer() {
   const { toggleDarkMode } = useTheme();
-  return <Topbar onToggleDarkMode={toggleDarkMode} />;
+  const { t } = useTranslation();
+
+  return <Topbar translate={t} onToggleDarkMode={toggleDarkMode} />;
 }
 
 export default TopbarContainer;
