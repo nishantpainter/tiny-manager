0bfa692bc6be5d0b61fa69c1c606699d3193049a
Nishant Painter <virtual.nishantpainter@gmail.com>
2021-05-04 08:14:57 +0530

    Organize imports for note

--- src/TinyManager/containers/Notes/index.js
+++ src/TinyManager/containers/Notes/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Box, TextField, Typography, IconButton } from "@material-ui/core";
-import {
-  SaveAlt as DownloadIcon,
-  DeleteForever as ClearIcon,
-} from "@material-ui/icons";
 import debounce from "lodash.debounce";
+import Box from "@material-ui/core/Box";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+
+import DownloadIcon from "@material-ui/icons/SaveAlt";
+import ClearIcon from "@material-ui/icons/DeleteForever";
 
 import Loader from "TinyManager/components/Loader";
 import TinyManagerAPI from "TinyManager/services/TinyManagerAPI";
