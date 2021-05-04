35c1c185bd8b12a0654f6cc595d1d7e2abfb58a3
Nishant Painter <virtual.nishantpainter@gmail.com>
2021-05-04 08:10:49 +0530

    Add use dialog hook

---
+++ src/TinyManager/hooks/useDialog.js
@@ -0,0 +1,18 @@
+import { useCallback, useMemo, useState } from "react";
+
+function useDialog() {
+  const [open, setOpen] = useState(false);
+
+  const openDialog = useCallback(() => setOpen(true), []);
+  const closeDialog = useCallback(() => setOpen(false), []);
+  const toggleDialog = useCallback(() => setOpen((open) => !open), []);
+
+  const value = useMemo(
+    () => ({ open, openDialog, closeDialog, toggleDialog }),
+    [open, openDialog, closeDialog, toggleDialog]
+  );
+
+  return value;
+}
+
+export default useDialog;
