b1960a2636063f3e02a2c75b938fb07d8d8d9a45
Nishant Painter <virtual.nishantpainter@gmail.com>
2021-05-05 08:37:36 +0530

    Update code structure

--- src/TinyManager/hooks/useDialog.js
+++ src/TinyManager/hooks/useDialog.js
@@ -3,9 +3,17 @@ import { useCallback, useMemo, useState } from "react";
 function useDialog() {
   const [open, setOpen] = useState(false);
 
-  const openDialog = useCallback(() => setOpen(true), []);
-  const closeDialog = useCallback(() => setOpen(false), []);
-  const toggleDialog = useCallback(() => setOpen((open) => !open), []);
+  const openDialog = useCallback(() => {
+    setOpen(true);
+  }, []);
+
+  const closeDialog = useCallback(() => {
+    setOpen(false);
+  }, []);
+
+  const toggleDialog = useCallback(() => {
+    setOpen((open) => !open);
+  }, []);
 
   const value = useMemo(() => [open, openDialog, closeDialog, toggleDialog], [
     open,
