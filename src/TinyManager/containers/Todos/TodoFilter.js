10e97715a6193594eb454635bddc536f3daf3a73
Nishant Painter <virtual.nishantpainter@gmail.com>
2021-05-07 08:18:59 +0530

    Add todo filter label

--- src/TinyManager/containers/Todos/TodoFilter.js
+++ src/TinyManager/containers/Todos/TodoFilter.js
@@ -5,6 +5,7 @@ import MenuItem from "@material-ui/core/MenuItem";
 
 import { FILTERS } from "./index";
 import { Translate } from "TinyManager/providers/TranslationProvider";
+import { FormControl, InputLabel } from "@material-ui/core";
 function TodoFilter(props) {
   const { value, onChange, className } = props;
 
@@ -15,24 +16,30 @@ function TodoFilter(props) {
   };
 
   return (
-    <Select
-      size="small"
-      margin="dense"
-      variant="outlined"
-      value={value}
-      onChange={handleChange}
-      className={className}
-    >
-      {Object.keys(FILTERS).map((key) => (
-        <MenuItem key={FILTERS[key]} value={FILTERS[key]}>
-          <Translate>{key}</Translate>
-        </MenuItem>
-      ))}
-    </Select>
+    <FormControl margin="dense" variant="outlined">
+      <InputLabel id="todo-filter-by">Filter By</InputLabel>
+      <Select
+        label="Filter By"
+        labelId="todo-filter-by"
+        size="small"
+        margin="dense"
+        variant="outlined"
+        value={value}
+        onChange={handleChange}
+        className={className}
+      >
+        {Object.keys(FILTERS).map((key) => (
+          <MenuItem key={FILTERS[key]} value={FILTERS[key]}>
+            <Translate>{key}</Translate>
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
   );
 }
 
 TodoFilter.propTypes = {
+  label: PropTypes.string,
   value: PropTypes.string,
   className: PropTypes.string,
   onChange: PropTypes.any,
