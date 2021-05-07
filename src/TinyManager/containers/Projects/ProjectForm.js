9e3210fb3a4f7fbcc6b31a374b489f986eedd709
Nishant Painter <virtual.nishantpainter@gmail.com>
2021-05-07 08:31:25 +0530

    Update code structure

--- src/TinyManager/containers/Projects/ProjectForm.js
+++ src/TinyManager/containers/Projects/ProjectForm.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React from "react";
 import { useFormik } from "formik";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
@@ -21,9 +21,6 @@ function ProjectFormContainer(props) {
   const classes = useStyles();
   const { t } = useTranslation();
 
-  const handleCancel = useCallback(() => onCancel(), [onCancel]);
-  const handleSubmit = useCallback((values) => onSubmit(values), [onSubmit]);
-
   const formik = useFormik({
     initialValues: merge({ name: "", description: "" }, initialValues),
     validate: (values) => {
@@ -36,7 +33,7 @@ function ProjectFormContainer(props) {
       return errors;
     },
     onSubmit: (values, { setSubmitting }) => {
-      handleSubmit(values);
+      onSubmit(values);
       setSubmitting(false);
     },
   });
@@ -47,7 +44,7 @@ function ProjectFormContainer(props) {
         translate={t}
         values={formik.values}
         errors={formik.errors}
-        onCancel={handleCancel}
+        onCancel={onCancel}
         onChange={formik.handleChange}
         onSubmit={formik.handleSubmit}
         disabled={formik.isSubmitting}
