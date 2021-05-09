5f43959cc4d8ffe86d8f666da905c4e3904a2f29
Nishant Painter <virtual.nishantpainter@gmail.com>
2021-05-09 10:12:31 +0530

    Configure snackbar provider

--- src/TinyManager/index.js
+++ src/TinyManager/index.js
@@ -4,13 +4,16 @@ import { HashRouter } from "react-router-dom";
 import Content from "TinyManager/containers/Content";
 import ThemeProvider from "TinyManager/providers/ThemeProvider";
 import TranslationProvider from "TinyManager/providers/TranslationProvider";
+import SnackbarProvider from "TinyManager/providers/SnackbarProvider";
 
 function TinyManager() {
   return (
     <ThemeProvider>
       <HashRouter>
         <TranslationProvider>
-          <Content />
+          <SnackbarProvider>
+            <Content />
+          </SnackbarProvider>
         </TranslationProvider>
       </HashRouter>
     </ThemeProvider>
