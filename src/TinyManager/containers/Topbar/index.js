4998bd21bf4f4378a77a25f7a0a0106bf4104031
Nishant Painter <virtual.nishantpainter@gmail.com>
2021-05-16 10:52:34 +0530

    Configure language selection

--- src/TinyManager/containers/Topbar/index.js
+++ src/TinyManager/containers/Topbar/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useMemo } from "react";
 
 import Topbar from "TinyManager/components/Topbar";
 import { useTheme } from "TinyManager/providers/ThemeProvider";
@@ -6,9 +6,37 @@ import { useTranslation } from "TinyManager/providers/TranslationProvider";
 
 function TopbarContainer() {
   const { toggleDarkMode } = useTheme();
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
 
-  return <Topbar translate={t} onToggleDarkMode={toggleDarkMode} />;
+  const handleLanguageChange = useCallback(
+    (event, lang) => {
+      i18n.changeLanguage(lang);
+    },
+    [i18n]
+  );
+
+  const languages = useMemo(
+    () => [
+      {
+        label: "English",
+        value: "en",
+      },
+      {
+        label: "French",
+        value: "fr",
+      },
+    ],
+    []
+  );
+
+  return (
+    <Topbar
+      translate={t}
+      onToggleDarkMode={toggleDarkMode}
+      onlanguageChange={handleLanguageChange}
+      languages={languages}
+    />
+  );
 }
 
 export default TopbarContainer;
