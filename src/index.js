2254d9497762ea8d1f38782160d4267ad4566198
Nishant Painter <virtual.nishantpainter@gmail.com>
2021-05-27 09:04:02 +0530

    Fix import

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 import TinyManager from "TinyManager";
 import * as serviceWorker from "serviceWorker";
 
-import "index.css";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
