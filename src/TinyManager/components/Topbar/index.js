39145a923d305cff4e6f34e76807a039ca49117e
Nishant Painter <virtual.nishantpainter@gmail.com>
2021-05-27 08:51:36 +0530

    Update code structure

--- src/TinyManager/components/Topbar/index.js
+++ src/TinyManager/components/Topbar/index.js
@@ -26,15 +26,36 @@ const useStyles = makeStyles({
   },
 });
 
-function Topbar(props) {
-  const {
-    onToggleDarkMode,
-    languages = [],
-    onlanguageChange,
-    translate,
-  } = props;
+function DarkThemeButton(props) {
+  const { onClick } = props;
+  return (
+    <IconButton
+      icon="invertColors"
+      size="small"
+      color="primary"
+      onClick={onClick}
+    ></IconButton>
+  );
+}
 
-  const classes = useStyles();
+DarkThemeButton.propTypes = {
+  onClick: PropTypes.func,
+};
+
+function GithubIconButton() {
+  return (
+    <IconButton
+      color="inherit"
+      icon="github"
+      component={MuiLink}
+      href="https://github.com/nishantpainter/tiny-manager"
+      target="_blank"
+    />
+  );
+}
+
+function LanguageSelection(props) {
+  const { languages, onChange } = props;
 
   const [languageAnchorEl, setLanguageAnchorEl] = useState(null);
 
@@ -47,11 +68,56 @@ function Topbar(props) {
   };
 
   const handleLanguageChange = (language) => (event) => {
-    if (onlanguageChange) {
-      onlanguageChange(event, language);
+    if (onChange) {
+      onChange(event, language);
       closeLanguageSelection();
     }
   };
+
+  return (
+    <>
+      <IconButton
+        icon="language"
+        size="small"
+        color="primary"
+        onClick={openLanguageSelection}
+        disabled={!languages.length}
+      ></IconButton>
+      <Menu
+        id="language-selection"
+        anchorEl={languageAnchorEl}
+        keepMounted
+        open={Boolean(languageAnchorEl)}
+        onClose={closeLanguageSelection}
+      >
+        {languages.map(({ value, label }) => (
+          <MenuItem onClick={handleLanguageChange(value)} key={value}>
+            {label}
+          </MenuItem>
+        ))}
+      </Menu>
+    </>
+  );
+}
+
+LanguageSelection.propTypes = {
+  languages: PropTypes.array,
+  onChange: PropTypes.func,
+};
+
+LanguageSelection.defaultProps = {
+  languages: [],
+};
+
+function Topbar(props) {
+  const {
+    onToggleDarkMode,
+    languages = [],
+    onlanguageChange,
+    translate,
+  } = props;
+
+  const classes = useStyles();
 
   return (
     <AppBar color="default">
@@ -74,41 +140,11 @@ function Topbar(props) {
         <Button color="primary" size="large" component={Link} to="/projects">
           {translate("Projects")}
         </Button>
+        <DarkThemeButton onClick={onToggleDarkMode} />
         &nbsp;
-        <IconButton
-          icon="invertColors"
-          size="small"
-          color="primary"
-          onClick={onToggleDarkMode}
-        ></IconButton>
-        <IconButton
-          icon="language"
-          size="small"
-          color="primary"
-          onClick={openLanguageSelection}
-          disabled={!languages.length}
-        ></IconButton>
-        <Menu
-          id="language-selection"
-          anchorEl={languageAnchorEl}
-          keepMounted
-          open={Boolean(languageAnchorEl)}
-          onClose={closeLanguageSelection}
-        >
-          {languages.map(({ value, label }) => (
-            <MenuItem onClick={handleLanguageChange(value)} key={value}>
-              {label}
-            </MenuItem>
-          ))}
-        </Menu>
+        <LanguageSelection languages={languages} onChange={onlanguageChange} />
         &nbsp;
-        <IconButton
-          color="inherit"
-          icon="github"
-          component={MuiLink}
-          href="https://github.com/nishantpainter/tiny-manager"
-          target="_blank"
-        />
+        <GithubIconButton />
       </Toolbar>
     </AppBar>
   );
